31992573e154fbb01a5976ce0b6910a2db985eae
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-20 21:30:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  const statusPara = document.getElementById("status");
+  const enterBtn = document.getElementById("enterBtn");
+
+  enterBtn.addEventListener("click", () => {
+    statusPara.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+  });
+});
