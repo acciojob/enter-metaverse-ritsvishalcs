6c4e3e9a7274bf020e3e5311d84e5a18b8ce7495
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-20 21:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-//your JS code here. If required.
 document.addEventListener("DOMContentLoaded", () => {
   const statusPara = document.getElementById("status");
   const enterBtn = document.getElementById("enterBtn");
 
   enterBtn.addEventListener("click", () => {
-    statusPara.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+    statusPara.innerHTML = "<h1>Entered Metaverse</h1>";
   });
 });
